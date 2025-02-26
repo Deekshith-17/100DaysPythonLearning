68b1ce93ac0c588c4b2c49102cae5ed8b521bd6c
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-02-26 12:13:44 +0000

    For Loop Programs

---
+++ PythonBasics/For_loop.py
@@ -0,0 +1,19 @@
+# 1
+
+# for n in range(2,21,2):
+#     print(n)
+
+# 2
+
+# for n in range(0,20,5):
+#      print(n)
+
+# 3
+
+# for i in range(1,11):
+#      print(i)
+
+#  4
+
+# for i in range(5,11):
+#      print(i)
