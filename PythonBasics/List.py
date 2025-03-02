37c2d068e245bbce90e3e084d733a8b591b794ee
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-02 07:16:49 +0000

    List Method

---
+++ PythonBasics/List.py
@@ -0,0 +1,30 @@
+# 1
+
+# L1=[]
+# L2=[1,2,3.5,4,5]
+# L3=["mat","phy","che","cs"]
+# L4=["car","bus","bike"]
+# print(len(L1))
+# print(len(L2))
+# print(len(L3))
+# print(len(L4))
+
+# 2
+
+# L1=[1,2,3,4]
+# print(len(L1))
+
+# 3 Negative indexing
+
+# L1 = [1, 2, 3, 4, 5, 6]
+# print(L1[-5])
+# print(len(L1))
+
+
+# 4 positive indexing
+
+# L1 = [1, 2, 3, 4, 5, 6]
+# print(L1[4])
+# print(len(L1))
+
+
