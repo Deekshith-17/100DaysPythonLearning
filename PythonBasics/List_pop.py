76649c2dc7f8d6712a8f2872b179072d2e7ba243
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-03 13:12:49 +0000

    List for Decrease

--- PythonBasics/List_pop.py
+++ PythonBasics/List_pop.py
@@ -1,5 +1,10 @@
-L = [1, 2, 3, 4, 5]
-L.pop(4)
-print(L)
-L.pop(3)
+# L = [1, 2, 3, 4, 5]
+# L.pop(4)
+# print(L)
+# L.pop(3)
+# print(L)
+
+L=[1,2,3,4,5,6]
+X=int(input("Enter number:"))
+L.pop(X)
 print(L)
