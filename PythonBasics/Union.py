72113e26609c9717c4d59d571868a45d58fee81e
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-06 11:20:40 +0000

    Union Method

---
+++ PythonBasics/Union.py
@@ -0,0 +1,14 @@
+# U1=[1,2,3,4]
+# U2=[5,6,7,8]
+# set1 = set(U1)
+# set2 = set(U2)
+# print(set1.union(set2))
+
+
+U1=[1,2,3,4]
+U2=[5,6,7,8]
+set1 = set(U1)
+set2 = set(U2)
+union_set = set1 | set2
+print(union_set)    
+
