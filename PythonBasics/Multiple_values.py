cac21a58cebb5a55856dc0118ad739249e058e0d
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-09 06:26:53 +0000

    Multiple Functions

---
+++ PythonBasics/Multiple_values.py
@@ -0,0 +1,8 @@
+def calculation(a,b):
+    addition=a+b
+    subtraction=a-b
+    return addition,subtraction
+a=int(input("Enter Value of a:"))
+b=int(input("Enter Value of b:"))
+result=calculation(a,b)
+print(result)
