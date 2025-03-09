2c30209fa40bf285e08ad8b1a11f75e53a92a49c
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-09 06:24:20 +0000

    Simple Interest

---
+++ PythonBasics/3Set_Si.py
@@ -0,0 +1,8 @@
+i=1
+while i<=3:
+    p=float(input("Enter Value of P :"))
+    n=int(input("Enter Value of N :"))
+    r=float(input("Enter Value of R :"))
+    SI=p*n*r/100
+    print("Simple interest:",SI)
+    i=i+1
