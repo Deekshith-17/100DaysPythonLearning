142527d7c96bd2a1de7e0f5be5cdc14c88ea041c
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-21 04:50:46 +0000

    Class Attributes

---
+++ PythonBasics/Instance_Attributes.py
@@ -0,0 +1,8 @@
+class Student:
+   def __init__(self,name,grade):
+       self.name=name
+       self.grade=grade
+       print("Name:",self.name,",Grade:",self.grade)
+student1= Student("Ram","B")
+student2= Student("Shyam","C")
+
