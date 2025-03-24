94188db01401ce151c6857c98932d8fae0ebc0c1
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-24 11:40:31 +0000

    Configuring Logging

---
+++ PythonBasics/Configuring_Logging.py
@@ -0,0 +1,23 @@
+import logging
+
+# Create logger
+logger = logging.getLogger('my_app')
+logger.setLevel(logging.DEBUG)  # Set global log level
+
+# Create console handler and set level to debug
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add console handler to logger
+logger.addHandler(console_handler)
+
+# Example usage
+logger.debug('This is a debug message')
+logger.info('This is an info message')
+logger.warning('This is a warning message')
+logger.error('This is an error message')
+logger.critical('This is a critical message')
