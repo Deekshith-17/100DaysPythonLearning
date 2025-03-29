0f86f2362d4b21a87969babe49b451c14278d225
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-03-29 05:43:39 +0000

    Random Function

---
+++ PythonBasics/random.rand().py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Generating a single random float
+random_float = np.random.rand()
+print("random_float:",random_float)
+
+# Generating a 1D array of random floats
+array_1d = np.random.rand(5)
+print("array_1d:",array_1d)
+
+# Generating a 2D array of random floats
+array_2d = np.random.rand(2, 3)
+print("array_2d:",array_2d)
+
+# Generating a 3D array of random floats
+array_3d = np.random.rand(2, 3, 4)
+print("array_3d:",array_3d)
