8eecb65b081fef5fc5a92cf03ecafdcb54da6cf4
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-04-07 11:49:10 +0000

    Multiplying Arrays with Different Data Types

---
+++ PythonBasics/Multiplying-Arrays_Different_Data-Types.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Creating arrays with different data types
+a = np.array([1.5, 2.5, 3.5])
+b = np.array([2, 3, 4], dtype=np.int32)
+
+# Performing multiplication
+result = a * b
+print(result)
