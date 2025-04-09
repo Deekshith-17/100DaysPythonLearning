094c37ae5cb74c16ec607d64990a71c9fa7f3931
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-04-09 01:16:03 +0000

    Division with Different Data Types

---
+++ PythonBasics/Division_Different_DataTypes.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Creating arrays with different data types
+a = np.array([10, 20, 30], dtype=np.int32)
+b = np.array([2.5, 5.0, 10.0], dtype=np.float64)
+
+# Performing division
+result = a / b
+print(result)
