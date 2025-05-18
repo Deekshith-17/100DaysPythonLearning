782d9fa6e69aebcb3e717132e63c1bed225b1f20
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-05-18 05:28:01 +0000

    Matrix Dot Product

---
+++ PythonBasics/dot() fun.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Define two matrices
+matrix_1 = np.array([[1, 2], [3, 4]])
+matrix_2 = np.array([[5, 6], [7, 8]])
+
+# Compute dot product
+matrix_product = np.dot(matrix_1, matrix_2)
+print(matrix_product)
