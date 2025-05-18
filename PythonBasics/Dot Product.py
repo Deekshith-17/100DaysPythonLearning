8eea90b9661004c096374d23b6d82329e389e172
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-05-18 05:27:24 +0000

    Using NumPy for Dot Product

---
+++ PythonBasics/Dot Product.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Define two vectors
+vector_1 = np.array([1, 2, 3])
+vector_2 = np.array([4, 5, 6])
+
+# Compute dot product
+dot_product = np.dot(vector_1, vector_2)
+print(dot_product)
