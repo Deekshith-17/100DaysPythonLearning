c38b42432381d519b2957e33845696ee557d59a8
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-06-04 11:57:40 +0000

    Arithmetic Operations on a Series with Scalar Value

---
+++ PythonBasics/Arithmetic Operations.py
@@ -0,0 +1,14 @@
+import pandas as pd
+s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
+
+# Display the Input Series
+print('Input Series\n',s)
+
+# Apply all Arithmetic Operation and Display the Results
+print('\nAddition:\n',s+2)
+print('\nSubtraction:\n', s-2)
+print('\nMultiplication:\n', s * 2)
+print('\nDivision:\n', s/2)
+print('\nExponentiation:\n', s**2)
+print('\nModulus:\n', s%2)
+print('\nFloor Division:\n', s//2)
