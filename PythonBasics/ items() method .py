11f8864eaab01cbe1e7e22d531c460c0008bcf2c
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-05 15:54:14 +0000

    Iterate Over Column Pairs

---
+++ PythonBasics/ items() method .py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+ 
+df = pd.DataFrame(np.random.randn(4,3),columns=['col1','col2','col3'])
+
+print("Original DataFrame:\n", df)
+
+# Iterate Through DataFrame rows
+print("Iterated Output:")
+for key,value in df.items():
+   print(key,value)
