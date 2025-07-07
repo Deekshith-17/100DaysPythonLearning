458ce19fad2c3f1d12177b4e7c11e0005daca6f4
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-07 12:04:39 +0000

    Iterating Through DataFrame Columns

---
+++ PythonBasics/DataFrame columns.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+ 
+N = 5
+df = pd.DataFrame({
+   'A': pd.date_range(start='2016-01-01', periods=N, freq='D'),
+   'x': np.linspace(0, stop=N-1, num=N),
+   'y': np.random.rand(N),
+   'C': np.random.choice(['Low', 'Medium', 'High'], N).tolist(),
+   'D': np.random.normal(100, 10, size=N).tolist()
+})
+
+print("Original DataFrame:\n", df)
+
+# Iterate Through DataFrame Columns
+print("Output:")
+for col in df:
+   print(col)
