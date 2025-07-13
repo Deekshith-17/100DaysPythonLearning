4d51456a48dc617ba29ff527ab01a97f61a263bb
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-13 11:19:27 +0000

    Using map() Function

---
+++ PythonBasics/ map() function.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.random.randn(5,3),columns=['col1','col2','col3'])
+
+# My custom function
+df['col1'].map(lambda x:x*100)
+print(df.apply(np.mean))
