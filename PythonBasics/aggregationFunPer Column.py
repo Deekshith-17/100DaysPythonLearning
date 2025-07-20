39ae9f5914033c10040ee46e000c05d938f266cd
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-20 15:33:12 +0000

    Applying Different aggregation Functions Per Column

---
+++ PythonBasics/aggregationFunPer Column.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame([[1, 2, 3, 1],
+                   [4, 5, 6, np.nan],
+                   [7, 8, 9, 2],
+                   [np.nan, 2, np.nan, 3]],
+   index = pd.date_range('1/1/2024', periods=4),
+   columns = ['A', 'B', 'C', 'D'])
+
+print("Input DataFrame:\n",df)
+result = df.agg({'A': ['sum', 'min'], 'B': ['min', 'max']})
+print("\nResults:\n",result)
