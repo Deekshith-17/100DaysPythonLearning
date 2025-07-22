d08a29a2712fa7995838d8f162339ed503b4341b
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-22 11:44:28 +0000

    The merge() Method in Pandas

---
+++ PythonBasics/merge operations.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Creating the first DataFrame
+left = pd.DataFrame({
+'id': [1, 2, 3, 4, 5],
+'Name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+'subject_id': ['sub1', 'sub2', 'sub4', 'sub6', 'sub5']
+})
+
+# Creating the second DataFrame
+right = pd.DataFrame({
+'id': [1, 2, 3, 4, 5],
+'Name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+'subject_id': ['sub2', 'sub4', 'sub3', 'sub6', 'sub5']
+})
+
+print("Left DataFrame:")
+print(left)
+print("\nRight DataFrame:")
+print(right)
