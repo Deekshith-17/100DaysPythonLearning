33482bbe3bde43e187be991ae220b18c5ea88c74
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-23 11:42:42 +0000

    Merge Two DataFrames on Multiple Keys

---
+++ PythonBasics/merge.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Creating the first DataFrame
+left = pd.DataFrame({'id': [1, 2, 3, 4, 5],
+'Name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+'subject_id': ['sub1', 'sub2', 'sub4', 'sub6', 'sub5']
+})
+
+# Creating the second DataFrame
+right = pd.DataFrame({
+'id':[1,2,3,4,5],
+'Name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+'subject_id':['sub2','sub4','sub3','sub6','sub5']})
+
+# Merging on multiple keys 'id' and 'subject_id'
+result = left.merge(right, on=['id', 'subject_id'])
+print(result)
