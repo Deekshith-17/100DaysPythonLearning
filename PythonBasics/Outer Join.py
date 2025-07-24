3a0609befa3d4bba1a25f4ae64584c08adbe7d90
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-07-24 10:48:15 +0000

    Outer join operation on two DataFrames

---
+++ PythonBasics/Outer Join.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Creating the first DataFrame
+left = pd.DataFrame({
+'id': [1, 2, 3, 4, 5],
+'Name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+'subject_id': ['sub1', 'sub2', 'sub4', 'sub6', 'sub5']
+})
+
+# Creating the second DataFrame
+right = pd.DataFrame({
+'id': [1, 2, 3, 4, 5],
+'Name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+'subject_id': ['sub2', 'sub4', 'sub3', 'sub6', 'sub5']
+})
+
+# Merging the DataFrames using the outer join 
+print(left.merge(right, how='outer', on='subject_id'))
