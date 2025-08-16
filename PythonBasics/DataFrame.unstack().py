0ec41b48c65914d96539af57c257097080ce88be
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-08-16 05:09:48 +0000

    Unstacking in Pandas

---
+++ PythonBasics/DataFrame.unstack().py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# Create MultiIndex
+tuples = [["x", "x", "y", "y", "", "f", "z", "z"],["1", "2", "1", "2", "1", "2", "1", "2"]]
+index = pd.MultiIndex.from_arrays(tuples, names=["first", "second"])
+
+# Create a DataFrame
+df = pd.DataFrame(np.random.randn(8, 2), index=index, columns=["A", "B"])
+
+# Display the input DataFrame
+print('Input DataFrame:\n', df)
+
+# Unstack the DataFrame
+unstacked = df.unstack()
+
+print('Output Reshaped DataFrame:\n', unstacked)
