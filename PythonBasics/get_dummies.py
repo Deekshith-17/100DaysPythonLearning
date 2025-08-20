f215760f72b266834a67ecc3d702bde40ceafd28
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-08-20 11:44:58 +0000

    Creating Dummy Variables with Prefix

---
+++ PythonBasics/get_dummies.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+# Create a DataFrame
+df = pd.DataFrame({"keys": list("aeeioou"), "values": range(7)})
+
+# Display the Input DataFrame
+print('Input DataFrame:\n',df)
+
+# Create dummy variables for the keys column
+dummies = pd.get_dummies(df["keys"], prefix="Col_")
+
+print('Resultant Dummy Variables with Prefix:\n',dummies)
