3c6e8fb3bd4e56e5ed87ff6c5ac45b3a2c5041e0
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-08-26 12:26:54 +0000

    Class Methods in Date Class

---
+++ PythonBasics/Date Class.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+print (date.today())
+d1=date.fromisoformat('2023-04-20')
+print (d1)
+d2=date.fromisoformat('20230420')
+print (d2)
+d3=date.fromisoformat('2023-W16-4')
+print (d3)
