e07e1cde4b9817fd5403ecb30b28c806cf717313
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-09-03 16:09:07 +0000

    Math Module - Special Methods

---
+++ PythonBasics/math module.py
@@ -0,0 +1,11 @@
+# Importing math Module
+import math
+
+# Using methods of math module
+print(math.sqrt(9))
+print(math.pow(3, 3))
+print(math.exp(1))
+print(math.log(100, 10))
+
+print(math.factorial(4))
+print(math.gcd(12, 3))
