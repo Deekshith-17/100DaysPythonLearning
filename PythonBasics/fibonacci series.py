d60bb0e17323498688b56d0919d8ad98be27bf74
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-09-12 10:55:32 +0000

    Normal function

---
+++ PythonBasics/fibonacci series.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+   fibo = []
+   a, b = 0, 1
+   while True:
+      c=a+b
+      if c>=n:
+         break
+      fibo.append(c)
+      a, b = b, c
+   return fibo
+f = fibonacci(10)
+for i in f:
+   print (i)
