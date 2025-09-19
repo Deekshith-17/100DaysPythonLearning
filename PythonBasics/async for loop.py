ec8d6d514c336acc270067891fbdc4e1f47370f3
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-09-19 16:49:30 +0000

    Asynchronous Generator

---
+++ PythonBasics/async for loop.py
@@ -0,0 +1,12 @@
+import asyncio
+
+async def async_generator(x):
+   for i in range(1, x+1):
+      await asyncio.sleep(1)
+      yield i
+      
+async def main():
+   async for item in async_generator(5):
+      print(item)
+      
+asyncio.run(main())
