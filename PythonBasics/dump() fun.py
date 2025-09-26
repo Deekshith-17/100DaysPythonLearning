1c4df2648625f90ee428978e9f31ddb9617b683e
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-09-26 16:42:31 +0000

    Serializing an Object

---
+++ PythonBasics/dump() fun.py
@@ -0,0 +1,9 @@
+import pickle
+
+data = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+
+# Open a file in binary write mode
+with open('data.pkl', 'wb') as file:
+   # Serialize the data and write it to the file
+   pickle.dump(data, file)
+   print ("File created!!")   
