598edf74b62990a1387f61e7a6f6f8fb43482a6c
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-10-12 04:04:08 +0000

    Docstrings for Modules

---
+++ PythonBasics/docstring_module.py
@@ -0,0 +1,21 @@
+import os
+
+"""
+This module provides Utility functions for file handling operations.
+
+Functions:
+- 'read_file(filepath)': Reads and returns the contents of the file.
+- 'write_file(filepath, content)': Writes content to the specified file.
+
+Classes:
+- 'FileNotFoundError': Raised when a file is not found.
+
+Example usage:
+
+   >>> import file_utils
+   >>> content = file_utils.read_file("example.txt")
+   >>> print(content)
+   'Hello, world!'
+   >>> file_utils.write_file("output.txt", "This is a test.")
+"""
+print("This is os module")
